dc6f27241660bda4bb9b95609fba0ee884f0879a
nawo6TGJD <129091686+nawo6TGJD@users.noreply.github.com>
2025-11-21 23:55:53 +0530

    changes

--- Platto/src/App.jsx
+++ Platto/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import NavBar from './components/NavBar/NavBar'
 import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+
 
 const App = () => {
   return (
-    <div>
-      <div className='app'> </div>
+    
+      <div className='app'> 
       <NavBar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
--- Platto/src/components/Header/Header.jsx
+++ Platto/src/components/Header/Header.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes
+          crafted with the finest ingredients.
+        </p>
+        <button>View Menu</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
